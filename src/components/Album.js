af3bfb42ddb07a994f77eb46c15d7d3e3184e02c
cfierke <christopherfierke@gmail.com>
2019-05-14 12:52:44 -0500

    Checkpoint 8 player Bar Buttons: Completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
+import PlayerBar from './PlayerBar';
 
 class Album extends Component {
   constructor(props) {
@@ -43,6 +44,14 @@ class Album extends Component {
       if (!isSameSong) { this.setSong(song); }
       this.play();
     }
+  }
+
+  handlePrevClick() {
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = Math.max(0, currentIndex - 1);
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play();
   }
 
   getPlayButton(index, song) {
@@ -98,7 +107,7 @@ class Album extends Component {
               onClick={() => this.handleSongClick(song)}
             >
               <td
-                onMouseEnter={() => this.handleOnMouseEnter(index, )}
+                onMouseEnter={() => this.handleOnMouseEnter(index)}
                 onMouseLeave={() => this.handleOnMouseLeave()}
               >{this.getPlayButton(index, song)}</td>
               <td>{song.title}</td>
@@ -108,6 +117,12 @@ class Album extends Component {
           }
           </tbody>
         </table>
+        <PlayerBar
+          isPlaying={this.state.isPlaying}
+          currentSong={this.state.currentSong}
+          handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+          handlePrevClick={() => this.handlePrevClick()}
+        />
       </section>
     );
   }
